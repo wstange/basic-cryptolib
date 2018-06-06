6186f39b31b39ff99a33fcb9a09395be6733ba10
wstange <wstange@uni-potsdam.de>
2018-06-06 15:58:43 +0200

    added gcd, added dependencies

--- utils/arithmetic/common.py
+++ utils/arithmetic/common.py
@@ -1,7 +1,12 @@
+# extended greatest common divisor
 def egcd(a, b):
     if(a < 0): a = a%b
-    if a == 0:
-        return (b, 0, 1)
+    if a == 0: return (b, 0, 1)
     else:
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
+
+# greatest common divisor
+def gcd(a, b):
+	while b != 0: a, b = b, a % b
+	return a
--- utils/arithmetic/mod.py
+++ utils/arithmetic/mod.py
@@ -1,5 +1,7 @@
+import common
+
 def modinv(a, m):
-    g, x, y = egcd(a, m)
+    g, x, y = common.egcd(a, m)
     if g != 1:
         raise Exception('modular inverse does not exist')
     else:
